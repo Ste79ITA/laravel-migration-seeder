7221c528c86cde0f7812e0922248706237d0cd5d
Ste79ITA <nastri.s@gmail.com>
2023-12-13 17:32:17 +0100

    added web page with controller and Trains model

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', "TRAIN'S PAGE")
+
+@section('content')
+<section class="py-5">
+<div class="container ">
+    <h1 class="title py-5">TRAIN'S PAGE</h1>
+
+    <div class="trains">
+        
+
+       
+    </div>
+</div>
+</section>
+@endsection
